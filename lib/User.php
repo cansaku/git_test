5c254c3ac048b386a68970c01168b8edd80fdd51
s yoshikawa <sakucan2007@yahoo.co.jp>
2019-03-31 22:53:27 +0700

    I added constructor to the User class

--- lib/User.php
+++ lib/User.php
@@ -2,12 +2,17 @@
 
 class User {
 
- private $id;
- private $name;
- private $age;
- private $email;
+	private $id;
+	private $name;
+	private $age;
+	private $email;
 
-
+	public function __construct($obj){
+		$this->id = $obj->id;
+		$this->name = $obj->name;
+		$this->age = $obj->age;
+		$this->email = $obj->email;
+	}
 
 }
 
